6a9bf7bade081532d5ee214e72f731267e40a194
JustinMShea <jshea@mail.roosevelt.edu>
2017-08-26 01:32:40 -0500

    script to roxygenize build vignette and render.

---
+++ data-raw/Final-items.R
@@ -0,0 +1,14 @@
+
+###############
+# Final items #
+###############
+
+# time to roxygenize those .R description files we wrote!
+devtools::document()
+
+# Build a vignette template
+devtools::use_vignette()
+
+# Render .pdf and .html vignettes
+library(rmarkdown)
+rmarkdown::render("vignettes/Hamilton-Leff Filter vignette.Rmd", "all")
