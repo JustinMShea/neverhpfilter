2b586fc2a5924602e368b4af5dd53abd19c66889
JustinMShea <jshea@mail.roosevelt.edu>
2017-09-19 07:49:38 -0500

    added "h" look ahead parameter to function

--- R/HLfilter.R
+++ R/HLfilter.R
@@ -1,32 +1,32 @@
 # Hamilton Filter
 library(xts)
-HLfilter.lm <- function(x) {
+HLfilter.lm <- function(x, h = 8) {
 
-        DF <- merge(lag.xts(x, k = -8, na.pad = TRUE),
+        DF <- merge(lag.xts(x, k = -h, na.pad = TRUE),
                     x,
                     lag.xts(x, k = 1, na.pad = TRUE),
                     lag.xts(x, k = 2, na.pad = TRUE),
                     lag.xts(x, k = 3, na.pad = TRUE))
 
-        colnames(DF) <- c("x8", "x", "x_1", "x_2", "x_3")
+        colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
 
-        HL_filter <- lm(x8 ~ x + x_1 + x_2 + x_3, data = DF)
+        HL_filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
 
         HL_filter
 
 }
 
-HLfilter <- function(x) {
+HLfilter <- function(x, h = 8) {
 
         DF <- merge(x,
-                    lag.xts(x, k = 8, na.pad = TRUE),
-                    lag.xts(x, k = 9, na.pad = TRUE),
-                    lag.xts(x, k = 10, na.pad = TRUE),
-                    lag.xts(x, k = 11, na.pad = TRUE))
+                    lag.xts(x, k = h, na.pad = TRUE),
+                    lag.xts(x, k = h+1, na.pad = TRUE),
+                    lag.xts(x, k = h+2, na.pad = TRUE),
+                    lag.xts(x, k = h+3, na.pad = TRUE))
 
-        colnames(DF) <- c("x", "x_8", "x_9", "x_10", "x_11")
+        colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
 
-        HL_filter <- lm(x ~ x_8 + x_9 + x_10 + x_11, data = DF)
+        HL_filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
 
 
         HL_fit <- as.xts(unname(HL_filter$fitted.values),
@@ -36,7 +36,7 @@ HLfilter <- function(x) {
                            order.by = as.yearqtr(names(HL_filter$residuals)))
 
 
-        merge(DF$x, DF$x-DF$x_8, HL_fit, HL_resid)
+        merge(DF$x_h, DF$x_h-DF$x, HL_fit, HL_resid)
 
 }
 
