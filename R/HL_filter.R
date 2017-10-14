0393654015369c66b4d6f73662f5aca88cbc558d
JustinMShea <jshea@mail.roosevelt.edu>
2017-10-14 00:23:57 -0500

    Function documentation working and roxygenized.

--- R/HL_filter.R
+++ R/HL_filter.R
@@ -1,14 +1,18 @@
-#` Hamilton Filtered quarterly xts object.
-#`
-#` \code{HL_filter} returns an xts object containing the original series,
-#`                  the difference between t+h and t, the fitted (trend) values, as well
-#`                  as the residual (cyclical) component.
-#`
-#`@param x An xts object of quarterly periodicity.
-#`
-#`@param h The look ahead parameter indicating the length of the AR(4) lag. Default to h = 8, or 8 quarters.
-#`
-HL_filter <- function(x, h = 8, ...) {
+#' Hamilton Filtered quarterly xts object.
+#'
+#' \code{HL_filter} returns an xts object containing the original series,
+#'                  the difference between t+h and t, the fitted (trend) values, as well
+#'                  as the residual (cyclical) component.
+#'
+#'@param x An xts object of quarterly periodicity.
+#'
+#'@param h The look ahead parameter indicating the length of the AR(4) lag. Default to h = 8, or 8 quarters.
+#'
+#'@examples
+#'\dontrun{
+#' HL_filter(PAYEMS h = 8)
+#'}
+HL_filter <- function(x, h = 8) {
 
                 if (!requireNamespace("pkg", quietly = TRUE)) {
                         stop("Pkg xts needed for this function to work. Please install it.",
@@ -17,21 +21,21 @@ HL_filter <- function(x, h = 8, ...) {
 
         # Transform data
         DF <- merge(x,
-                    lag.xts(x, k = h, na.pad = TRUE),
-                    lag.xts(x, k = h+1, na.pad = TRUE),
-                    lag.xts(x, k = h+2, na.pad = TRUE),
-                    lag.xts(x, k = h+3, na.pad = TRUE))
+                    xts::lag.xts(x, k = h, na.pad = TRUE),
+                    xts::lag.xts(x, k = h+1, na.pad = TRUE),
+                    xts::lag.xts(x, k = h+2, na.pad = TRUE),
+                    xts::lag.xts(x, k = h+3, na.pad = TRUE))
         colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
 
         # linear model data
-        HL_Filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
+        HL_Filter <- stats::lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
 
         # convert fitted and residuals to xts objects
-        HL_fit <- as.xts(unname(HL_Filter$fitted.values),
-                         order.by = as.yearqtr(names(HL_Filter$fitted.values)))
+        HL_fit <- xts::as.xts(unname(HL_Filter$fitted.values),
+                         order.by = zoo::as.yearqtr(names(HL_Filter$fitted.values)))
 
-        HL_resid <- as.xts(unname(HL_Filter$residuals),
-                           order.by = as.yearqtr(names(HL_Filter$residuals)))
+        HL_resid <- xts::as.xts(unname(HL_Filter$residuals),
+                           order.by = zoo::as.yearqtr(names(HL_Filter$residuals)))
 
         # merge together relevant components
         merge(DF$x_h, DF$x_h-DF$x, HL_fit, HL_resid)
--- R/HL_filter_lm.R
+++ R/HL_filter_lm.R
@@ -1,12 +1,19 @@
-#` Linear Model Hamilton Filtered quarterly xts object.
-#`
-#` \code{HL_filter_lm} returns a hamilton filtered linear model of class "formula"
-#`
-#`@param x An xts object of quarterly periodicity.
-#`
-#`@param h The look ahead parameter indicating the length of the AR(4) lag. Default to h = 8, or 8 quarters.
-#`
-#`@inheritParams lm
+#' Linear Model Hamilton Filtered quarterly xts object.
+#'
+#' \code{HL_filter_lm} returns a hamilton filtered linear model of class "formula"
+#'
+#'@param x An xts object of quarterly periodicity.
+#'
+#'@param h The look ahead parameter indicating the length of the AR(4) lag. Default to h = 8, or 8 quarters.
+#'
+#'@param ... see "lm"
+#'
+#' @inheritParams lm Inherits arguements from "stats::lm"
+#'
+#'@examples
+#'\dontrun{
+#' HL_filter_lm(GDPC96, h = 8)
+#'}
 HL_filter_lm <- function(x, h = 8, ...) {
 
         if (!requireNamespace("pkg", quietly = TRUE)) {
@@ -14,15 +21,15 @@ HL_filter_lm <- function(x, h = 8, ...) {
                      call. = FALSE)
         }
 
-  DF <- merge(lag.xts(x, k = -h, na.pad = TRUE),
-              x,
-              lag.xts(x, k = 1, na.pad = TRUE),
-              lag.xts(x, k = 2, na.pad = TRUE),
-              lag.xts(x, k = 3, na.pad = TRUE))
+  DF <- merge(xts::lag.xts(x, k = -h, na.pad = TRUE),
+                           x,
+              xts::lag.xts(x, k = 1, na.pad = TRUE),
+              xts::lag.xts(x, k = 2, na.pad = TRUE),
+              xts::lag.xts(x, k = 3, na.pad = TRUE))
 
   colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
 
-  HL_Filter <- lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
+  HL_Filter <- stats::lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
 
   HL_Filter
 
