3660a60c0f855d5c5e1e38152384c40d044b6e99
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-03 17:37:14 -0500

    removed ACF function and forecast package dependency.

--- R/Acf_acf.R
+++
@@ -1,9 +0,0 @@
-#' Autocorrelation wrapper function to `apply` over xts
-#'
-#'@param x Vector of ordered observations.
-#'
-#'
-Acf_acf <- function(x) {
-        y <- forecast::Acf(x, plot = FALSE)[["acf"]]
-        y
-}
