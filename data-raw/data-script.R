dfe1b14ef07a4b318b4002020fa627fb88e33531
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-05 19:15:01 -0600

    Included some data in the package. Add more later.

--- data-raw/data-script.R
+++ data-raw/data-script.R
@@ -11,3 +11,18 @@ GDPC96 <- as.xts(read.zoo(Real_Gross_Domestic_Product, skip = 12, index.column =
 Total_nonfarm_Payrolls   <- "https://fred.stlouisfed.org/data/PAYEMS.txt"
 PAYEMS <- as.xts(read.zoo(Total_nonfarm_Payrolls , sep = "", skip = 42, index.column = 1,
                                             header = TRUE, format = "%Y-%m-%d", FUN = as.yearmon))
+
+
+###################################################################
+# compression data sets to xz level 9 .Rdata files #
+###################################################################
+
+dir.create("data")
+
+dataset_list <- c("RGDP", "Employment_Establishment")
+
+for (i in dataset_list) {
+  
+  save(list = i, file = paste0("data/", i, ".RData"), compress = "xz", compression_level = 9)
+  
+}
