7e7d4bacbbbee3b65014e1a8c62db1032604435b
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-12 19:09:20 -0600

    updated file name.

--- data-raw/Final-items.R
+++ data-raw/Final-items.R
@@ -11,5 +11,5 @@ devtools::use_vignette()
 
 # Render .pdf and .html vignettes
 library(rmarkdown)
-rmarkdown::render("vignettes/Hamilton-Leff-Filter-vignette.Rmd", "all")
+rmarkdown::render("vignettes/Never-HP-Filter_Vignette.Rmd", "all")
 rmarkdown::render("vignettes/US_Economic_Data_Management.Rmd", "all")
