16afe461856da73cd5e5d8eb546b95e74db43a38
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-14 17:24:24 -0600

    update data to add column names

--- data-raw/data-script.R
+++ data-raw/data-script.R
@@ -4,14 +4,14 @@ library(xts)
 Real_Gross_Domestic_Product <- "https://research.stlouisfed.org/fred2/data/GDPC1.txt"
 GDPC1 <- as.xts(read.zoo(Real_Gross_Domestic_Product, skip = 17, index.column = 1,
                         header = TRUE, format = "%Y-%m-%d", FUN = as.yearqtr))
-
+colnames(GDPC1) <- "GDPC1"
 
 
 ### Employment Rate ###
 Total_nonfarm_Payrolls   <- "https://fred.stlouisfed.org/data/PAYEMS.txt"
 PAYEMS <- as.xts(read.zoo(Total_nonfarm_Payrolls , sep = "", skip = 42, index.column = 1,
                                             header = TRUE, format = "%Y-%m-%d", FUN = as.yearmon))
-
+colnames(PAYEMS) <- "PAYEMS"
 
 ###################################################################
 # compression data sets to xz level 9 .Rdata files #
