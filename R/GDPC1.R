ef14a70522d3a3477c64f77c4766996ec6798205
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-14 18:04:44 -0600

    starting adding data documentation

---
+++ R/GDPC1.R
@@ -0,0 +1,17 @@
+#' GDPC1
+#'
+#' Data loads lazily. Type data("GDPC1") into the console.
+#'
+#' @docType data
+#'
+#' @usage data("GDPC1")
+#'
+#' @format An xts object of 283 observations on Real GDP variables:
+#' \itemize{
+#'  \item GDPC1
+#' }
+#' @source \url{https://www.cengage.com/cgi-wadsworth/course_products_wp.pl?fid=M20b&product_isbn_issn=9781111531041}
+#' @examples  str(corn)
+"corn"
+ 
+ 
