03709ccf6e38edeb41b6775456ae4c853db4ee65
JustinMShea <jshea@mail.roosevelt.edu>
2017-11-15 08:57:15 -0600

    updated documentation for HL_filter_lm

--- R/HL_filter_lm.R
+++ R/HL_filter_lm.R
@@ -1,6 +1,12 @@
-#' Linear Model Hamilton Filtered quarterly xts object.
+#' Hamilton Filtered quarterly time series Linear Model
+#' 
+#' For time series of quarterly periodicity, Hamilton's solution can be described as an \eqn{AR(4)} process, 
+#' additionally lagged by \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} +\beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
+#' \deqn{\hat{v}_{t+h} = y_{t+h} + \hat{\beta}_0 + \hat{\beta}_1 y_t + \hat{\beta}_2 y_{t-1} + \hat{\beta}_3 y_{t-2} + \hat{\beta}_4 y_{t-3}}
 #'
-#' \code{HL_filter_lm} returns a hamilton filtered linear model of class "formula"
+#'\value{
+#' \strong\code{HL_filter_lm} returns a linear model of class "formula", like the output created by `stats::lm`.
+#'}
 #'
 #'@param x An xts object of quarterly periodicity.
 #'
@@ -8,13 +14,14 @@
 #'
 #'@param ... see "lm"
 #'
+#'@inheritParams stats::lm see "lm"
+#'
 #'@references James D. Hamilton. Why You Should Never Use the Hodrick-Prescott Filter.
 #'            NBER Working Paper No. 23429, Issued in May 2017.
 #'
 #'@examples
-#'\dontrun{
 #' HL_filter_lm(GDPC1, h = 8)
-#'}
+#' 
 #'@export
 HL_filter_lm <- function(x, h = 8, ...) {
 
