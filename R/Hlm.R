5e62743403a1acd320344488cdf53bd7eb03f13c
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-10 21:03:47 -0600

    added "p" parameters to allow users to adjust the number of lags.

--- R/Hlm.R
+++ R/Hlm.R
@@ -1,6 +1,6 @@
 #' Hamilton Filtered quarterly time series Linear Model
-#' 
-#' For time series of quarterly periodicity, Hamilton's solution can be described as an \eqn{AR(4)} process, 
+#'
+#' For time series of quarterly periodicity, Hamilton's solution can be described as an \eqn{AR(4)} process,
 #' additionally lagged by \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} +\beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
 #' \deqn{\hat{v}_{t+h} = y_{t+h} + \hat{\beta}_0 + \hat{\beta}_1 y_t + \hat{\beta}_2 y_{t-1} + \hat{\beta}_3 y_{t-2} + \hat{\beta}_4 y_{t-3}}
 #'
@@ -18,26 +18,21 @@
 #'            NBER Working Paper No. 23429, Issued in May 2017.
 #'
 #'@examples
-#' Hlm(GDPC1, h = 8)
-#' 
+#' yth_AR(GDPC1, h = 8)
+#'
 #'@export
-Hlm <- function(x, h = 8, ...) {
+yth_AR <- function(x, h = 8, p = 4, ...) {
 
-  #      if (!requireNamespace("xts", quietly = TRUE)) {
-   #             stop("Pkg xts needed for this function to work. Please install it.",
-    #                 call. = FALSE)
-     #   }
+        if(!"xts" %in% class(x)) {
+        stop("This function requires objects of type xts.")
+}
 
-  DF <- merge(xts::lag.xts(x, k = -h, na.pad = TRUE),
-                           x,
-              xts::lag.xts(x, k = 1, na.pad = TRUE),
-              xts::lag.xts(x, k = 2, na.pad = TRUE),
-              xts::lag.xts(x, k = 3, na.pad = TRUE))
-  
-  colnames(DF) <- c("x_h", "x", "x_1", "x_2", "x_3")
+        yth <- lag(x, k = c(-h, 0:(p-1)), na.pad = TRUE)
 
-  HL_Filter <- stats::lm(x_h ~ x + x_1 + x_2 + x_3, data = DF)
+        lagnames <- c(paste0("yt",h), paste0('Xt_',0:(p-1)))
 
-  HL_Filter
+        colnames(yth) <- lagnames
+
+        stats::lm(get(lagnames[1]) ~ ., data = yth)
 
 }
