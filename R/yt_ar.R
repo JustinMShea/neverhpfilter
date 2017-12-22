461d408e02f4b2ad36314699025180f16afbeaa6
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-21 21:08:11 -0600

    adding clarity to documentation

--- R/yt_ar.R
+++ R/yt_ar.R
@@ -1,14 +1,14 @@
 #' A better Hodrick-Prescott Filter
 #'
 #' For time series of quarterly periodicity, Hamilton suggests an \eqn{AR(4)} process,
-#' additionally lagged by \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} + \beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
+#' regressed against a look ahead of \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} + \beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
 #' \deqn{\hat{v}_{t+h} = y_{t+h} - \hat{\beta}_0 + \hat{\beta}_1 y_t + \hat{\beta}_2 y_{t-1} + \hat{\beta}_3 y_{t-2} + \hat{\beta}_4 y_{t-3}}
 #'
-#'@return \code{yt_ar} returns a linear model estimed by ordinary least squares of class "formula", identical to the output created by `stats::lm`.
+#'@return \code{yt_ar} returns a linear model estimed by ordinary least squares of class "formula".
 #'
-#'@param x An xts object.
+#'@param x An univariate xts series of zoo index class, such as "Date", "yearmon", or "yearqtr".
 #'
-#'@param h The look ahead parameter. Default to h = 8, or 8 quarters for two years.
+#'@param h The look ahead parameter. Default to h = 8, or 8 quarters for two years of look ahead data.
 #'
 #'@param p Idicating the number of lags. Default to p = 4, or 4 quarters for one year.
 #'
