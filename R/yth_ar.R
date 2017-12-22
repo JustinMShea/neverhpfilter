4ee9f40b03c4b3455f648b378d3554407ce3398c
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-22 15:55:36 -0600

    make naming conventions

--- R/yth_ar.R
+++ R/yth_ar.R
@@ -25,11 +25,20 @@
 #'@export
 yt_ar <- function(x, h = 8, p = 4, ...) {
 
-        if(!"xts" %in% class(x)) {
+               if(!"xts" %in% class(x)) {
 
-stop("This function requires an xts object.")
+stop(paste("Arguement 'x' be an object of type xts.", class(x), "is not an xts object"))
+
+        } else if(h %% 1 != 0) {
+
+stop(paste("Argument 'h' must be a whole number.", h, "is not a whole number."))
+
+        } else if(p %% 1 != 0) {
+
+stop(paste("Argument 'p' must be a whole number.", p, "is not a whole number."))
 
         } else {
+
 
                  data  <- lag(x, k = c(0, h:(h+p-1)), na.pad = TRUE)
 
