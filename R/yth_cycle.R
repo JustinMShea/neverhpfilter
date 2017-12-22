84d5b6fa5e4c8e16a7c2a052d03ea64e2c12642f
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-21 23:45:22 -0600

    yth cycle

---
+++ R/yth_cycle.R
@@ -0,0 +1,49 @@
+#' yth_cycle
+#'
+#' An alternative to the Hodrick-Prescott Filter
+#'
+#' For time series of quarterly periodicity, Hamilton suggests an \eqn{AR(4)} process,
+#' additionally lagged by \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} + \beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
+#' \deqn{\hat{v}_{t+h} = y_{t+h} - \hat{\beta}_0 + \hat{\beta}_1 y_t + \hat{\beta}_2 y_{t-1} + \hat{\beta}_3 y_{t-2} + \hat{\beta}_4 y_{t-3}}
+#'
+#'@return \code{yth_cycle} returns a xts object of the cyclical component, the residual of the yt_ar model.
+#'
+#'@param x An xts object.
+#'
+#'@param h The look ahead parameter. Default to h = 8, or 8 quarters for two years.
+#'
+#'@param p Idicating the number of lags. Default to p = 4, or 4 quarters for one year.
+#'
+#'@param ... additional argeuments of accepted by the model class see "lm"
+#'
+#'@inheritParams stats::lm see "lm"
+#'
+#'@references James D. Hamilton. "Why You Should Never Use the Hodrick-Prescott Filter".
+#'            NBER Working Paper No. 23429, Issued in May 2017.
+#'
+#'@examples
+#' yth_cycle(GDPC1, h = 8, p = 4)
+#'
+#'@export
+yth_cycle <- function(x, h = 8, p = 4, ...) {
+
+        if(!"xts" %in% class(x)) {
+
+                stop("This function requires an xts object.")
+
+        } else {
+
+                # run yth_ar(p) model and store results
+                data <- lag(x, k = c(0, h:(h+p-1)), na.pad = TRUE)
+                lagnames <- c(paste0("yt",h), paste0('Xt_',0:(p-1)))
+                colnames(data) <- lagnames
+                formula <- paste0(c(paste0(paste0("yt",h)," ~ Xt_0"), paste0('+ Xt_',1:(p-1))), collapse = " ")
+                neverHP <- stats::lm(formula, data = data)
+
+                # cycle xts object
+                cycle <- xts::as.xts(unname(neverHP$residuals),
+                                     order.by = get(paste0("as.",class(index(x))))(names(neverHP$residuals)))
+
+                print(cycle)
+        }
+}
