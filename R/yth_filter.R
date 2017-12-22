0e6585c5dcddb7fb9e32765387d121406a64a41c
JustinMShea <jshea@mail.roosevelt.edu>
2017-12-22 15:56:07 -0600

    rename and updates

--- R/yth_filter.R
+++ R/yth_filter.R
@@ -1,6 +1,8 @@
-#' Hamilton Filtered quarterly time series
+#' yth_filter
 #'
-#' For time series of quarterly periodicity, Hamilton's solution can be described as an \eqn{AR(4)} process,
+#' Filtered time series xts object
+#'
+#' For a time series of say, quarterly periodicity, Hamilton's solution can be described as an \eqn{AR(4)} process,
 #' additionally lagged by \eqn{h} periods. \deqn{y_{t+h} = \beta_0 + \beta_1 y_t + \beta_2 y_{t-1} +\beta_3 y_{t-2} + \beta_4 y_{t-3} + v_{t+h}}
 #' \deqn{\hat{v}_{t+h} = y_{t+h} + \hat{\beta}_0 + \hat{\beta}_1 y_t + \hat{\beta}_2 y_{t-1} + \hat{\beta}_3 y_{t-2} + \hat{\beta}_4 y_{t-3}}
 #'
