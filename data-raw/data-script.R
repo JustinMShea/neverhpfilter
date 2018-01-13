a16389cda6813b2a12b84c7147d801102cd6bb65
JustinMShea <jshea@mail.roosevelt.edu>
2018-01-13 12:09:04 -0600

    Added additional data

--- data-raw/data-script.R
+++ data-raw/data-script.R
@@ -1,4 +1,16 @@
 
+# Hamilton Table 2
+Hamilton_table_2 <- read.table(paste0(getwd(),"/data-raw/Hamilton-table-2.txt"),
+                               skip=1, row.names=1, stringsAsFactors=FALSE,
+                               col.names = c("cycle.sd", "gdp.cor",
+                                             "random.sd", "gdp.rand.cor",
+                                             "Sample"))
+
+# transform date sample column into xts compatible syntax.
+Hamilton_table_2$Sample <- gsub("-","/", Hamilton_table_2$Sample)
+Hamilton_table_2$Sample <- gsub(":", "-", Hamilton_table_2$Sample)
+
+
 library(xts)
 # Real GDP
 Real_Gross_Domestic_Product <- "https://research.stlouisfed.org/fred2/data/GDPC1.txt"
@@ -21,16 +33,6 @@ USREC <- as.xts(read.zoo(Recession_Indicators , sep = "", skip = 69, index.colum
 colnames(USREC) <- "USREC"
 
 
-# Hamilton Table 2
-Hamilton_table_2 <- read.table(paste0(getwd(),"/data-raw/Hamilton-table-2.txt"),
-                               skip=1, row.names=1, stringsAsFactors=FALSE,
-                               col.names = c("cycle.sd", "gdp.cor",
-                                             "random.sd", "gdp.rand.cor",
-                                             "Sample"))
-# transform date sample column into xts compatible syntax.
-Hamilton_table_2$Sample <- gsub("-","/", Hamilton_table_2$Sample)
-Hamilton_table_2$Sample <- gsub(":", "-", Hamilton_table_2$Sample)
-
 # Real Gross Private Domestic Investment
 GDPIC1 <- as.xts(read.zoo("https://fred.stlouisfed.org/data/GPDIC1.txt", skip = 13, index.column = 1,
                           header = TRUE, format = "%Y-%m-%d", FUN = as.yearqtr))
@@ -47,7 +49,7 @@ EXPGSC1 <- as.xts(read.zoo("https://fred.stlouisfed.org/data/EXPGSC1.txt", skip 
 IMPGSC1 <- as.xts(read.zoo("https://fred.stlouisfed.org/data/IMPGSC1.txt", skip = 13, index.column = 1,
                              header = TRUE, format = "%Y-%m-%d", FUN = as.yearqtr))
 
-# Real imports of goods and services
+# Real Government Consumption Expenditures and Gross Investment
 GCEC1 <- as.xts(read.zoo("https://fred.stlouisfed.org/data/GCEC1.txt", skip = 13, index.column = 1,
                             header = TRUE, format = "%Y-%m-%d", FUN = as.yearqtr))
 
@@ -61,6 +63,18 @@ UNRATENSA <- as.xts(read.zoo("https://fred.stlouisfed.org/data/UNRATENSA.txt",
 GDPDEF <- as.xts(read.zoo("https://fred.stlouisfed.org/data/GDPDEF.txt", skip = 15,
                           index.column = 1, header = TRUE, format = "%Y-%m-%d",
                           FUN = as.yearqtr))
+
+# 10-Year Treasury Constant Maturity Rate
+GS10 <- as.xts(read.zoo("https://fred.stlouisfed.org/data/GS10.txt", skip = 14,
+                          index.column = 1, header = TRUE, format = "%Y-%m-%d",
+                          FUN = as.yearmon))
+
+# Effective Federal Funds Rate
+FEDFUNDS <- as.xts(read.zoo("https://fred.stlouisfed.org/data/FEDFUNDS.txt", skip = 60,
+                        index.column = 1, header = TRUE, format = "%Y-%m-%d",
+                        FUN = as.yearmon))
+
+
 
 
 
