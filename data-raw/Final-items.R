ccfd5678d25947449763c0ffc11cb3dc6efb7558
JustinMShea <jshea@mail.roosevelt.edu>
2018-01-21 11:40:33 -0600

    delete irrelevant scripts

--- data-raw/Final-items.R
+++ data-raw/Final-items.R
@@ -8,8 +8,3 @@ devtools::document()
 
 # Build a vignette template
 devtools::use_vignette()
-
-# Render .pdf and .html vignettes
-library(rmarkdown)
-rmarkdown::render("vignettes/Never-HP-Filter_Vignette.Rmd", "all")
-rmarkdown::render("vignettes/US_Economic_Data_Management.Rmd", "all")
