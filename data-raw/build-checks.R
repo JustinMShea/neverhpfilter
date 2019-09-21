a94f3ffa23d5a6c06000b6e0d7b521bf34dc8a6e
JustinMShea <jshea@roosevelt.edu>
2019-09-21 18:31:11 -0500

    added notes next to rhub check defining platform

--- data-raw/build-checks.R
+++ data-raw/build-checks.R
@@ -12,13 +12,15 @@ rhub::validate_email()
 rhub::platforms()
 rhub::platforms()$name
 
-rhub::check(platform = rhub::platforms()$name[1])
-rhub::check(platform = rhub::platforms()$name[4])
-rhub::check(platform = rhub::platforms()$name[9])
-rhub::check(platform = rhub::platforms()$name[10])
-rhub::check(platform = rhub::platforms()$name[11])
-rhub::check(platform = rhub::platforms()$name[12])
+rhub::check(platform = rhub::platforms()$name[1]) # "debian-clang-devel" 
+rhub::check(platform = rhub::platforms()$name[2]) # "debian-gcc-devel"  
+rhub::check(platform = rhub::platforms()$name[7]) # "fedora-gcc-devel"  
+rhub::check(platform = rhub::platforms()$name[10]) # "linux-x86_64-rocker-gcc-san"
 
-rhub::check(platform = rhub::platforms()$name[16])
-rhub::check(platform = rhub::platforms()$name[17])
-rhub::check(platform = rhub::platforms()$name[20])
+rhub::check(platform = rhub::platforms()$name[11]) # "macos-elcapitan-release" 
+
+rhub::check(platform = rhub::platforms()$name[12]) # "solaris-x86-patched" 
+
+rhub::check(platform = rhub::platforms()$name[16]) # "windows-x86_64-devel"
+rhub::check(platform = rhub::platforms()$name[17]) # "windows-x86_64-devel-rtools4"
+rhub::check(platform = rhub::platforms()$name[20]) # "windows-x86_64-release"
