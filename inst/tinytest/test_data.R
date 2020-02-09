e7389eaadf9dc830d13dcf032a625fe1c549cca9
JustinMShea <jshea@roosevelt.edu>
2020-02-08 18:45:13 -0600

    test dimnames of all data objects of != NULL. See issue-1 on GH.

---
+++ inst/tinytest/test_data.R
@@ -0,0 +1,27 @@
+
+
+expect_equal(class(names(EXPGSC1)), "character", info = "EXPGSC1 dimnames not NULL")
+
+expect_equal(class(names(FEDFUNDS)), "character", info = "FEDFUNDS dimnames not NULL")
+
+expect_equal(class(names(GCEC1)), "character", info = "GCEC1 dimnames not NULL")
+
+expect_equal(class(names(GDPDEF)), "character", info = "GDPDEF dimnames not NULL")
+
+expect_equal(class(names(GPDIC1)), "character", info = "GPDIC1 dimnames not NULL")
+
+expect_equal(class(names(GS10)), "character", info = "GS10 dimnames not NULL")
+
+expect_equal(class(names(Hamilton_table_2)), "character", info = "Hamilton dimnames not NULL")
+
+expect_equal(class(names(IMPGSC1)), "character", info = "IMPGSC1 dimnames not NULL")
+
+expect_equal(class(names(PAYEMS)), "character", info = "PAYEMS dimnames not NULL")
+
+expect_equal(class(names(PCECC96)), "character", info = "PCECC96 dimnames not NULL")
+ 
+expect_equal(class(names(SP500)), "character", info = "SP500 dimnames not NULL")
+
+expect_equal(class(names(UNRATENSA)), "character", info = "UNRATENSA dimnames not NULL")
+
+expect_equal(class(names(USREC)), "character", info = "USREC dimnames not NULL")
