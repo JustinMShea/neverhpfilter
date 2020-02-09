f50a475034e70770ca3b9059a4471aedaf93b18b
JustinMShea <jshea@roosevelt.edu>
2020-02-08 18:45:36 -0600

    make sure preprocessing data object has 5 vars

--- inst/tinytest/test_yth_glm.R
+++ inst/tinytest/test_yth_glm.R
@@ -2,3 +2,5 @@
 GDPC1_model <- yth_glm(GDPC1, h = 8, p = 4)
 
 expect_equivalent(class(GDPC1_model)[1], "glm", info = "The yth_glm model is of class 'glm'")
+
+expect_equivalent(length(names(GDPC1_model$data)), 5, info = "yth_glm data object contains vars")
